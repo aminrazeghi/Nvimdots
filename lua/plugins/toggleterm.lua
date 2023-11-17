c02b800fa9aa81778f3b917e3d358bad346eb628
amin razeghi <mnrzghi@gmail.com>
2023-11-17 15:09:37 +0600

    plugins  Mardown-preview fixed

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,16 +2,4 @@ return {
 
   { "akinsho/toggleterm.nvim", version = "*", config = true },
   { "David-Kunz/gen.nvim" },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-    init = function()
-      vim.g.mkdp_theme = "dark"
-      vim.g.mkdp_browser = "brave"
-    end,
-  },
 }
